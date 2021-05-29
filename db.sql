e4199860451d2f0bc8984debea9030da3428436f
Adam Ates (s3719192) <80433492+stirfrychicken@users.noreply.github.com>
2021-05-29 17:38:15 +1000

    Created shifts and staff_shifts table

--- db.sql
+++ db.sql
@@ -1,3 +1,10 @@
+-- This query creates the necessary tables for this website to function
+-- and should be run once if you just downloaded the files.
+-- It also seeds the database with a basic admin account to get you
+-- logged in and started
+-- Start xampp and navigate to localhost/phpmyadmin/
+-- Click the SQL tab, copy paste the contents of this file and click "Go"
+
 create database work;
 use work;
 
@@ -13,3 +20,14 @@ address VARCHAR(255),
 role ENUM('none', 'manager') DEFAULT 'none'
 );
 
+CREATE TABLE shifts(
+shift_id INT AUTO_INCREMENT PRIMARY KEY,
+day VARCHAR(20),
+start DATETIME,
+end DATETIME
+);
+
+CREATE TABLE staff_shifts(
+staff_id INT REFERENCES staff (staff_id),
+shift_id INT REFERENCES shift (shift_id)
+);
